158a94a14bf2dbcf893bcb64aa31530f283652cc
Lalo Luna <thelalox789@gmail.com>
2018-04-15 18:15:34 -0500

    Primer Commit
    Primer commit del proyecto

---
+++ assets/js/other2.js
@@ -0,0 +1,327 @@
+$(document).ready(function(){
+    //04
+    $("#videoLeccion_04_1").on("click", function(){
+        $("#video").attr("src", "sistema_serna2/SISTEMA SERNA_I/VIDEOS/Nivel_04/04/01_SS_Escuchar_SOL_04.webm");
+    });
+    $("#videoLeccion_04_2").on("click", function(){
+        $("#video").attr("src", "sistema_serna2/SISTEMA SERNA_I/VIDEOS/Nivel_04/04/02_SS_Cantar_SOL_04.webm");
+    });
+    $("#videoLeccion_04_3").on("click", function(){
+        $("#video").attr("src", "sistema_serna2/SISTEMA SERNA_I/VIDEOS/Nivel_04/04/03_SS_Escuchar_FA_04.webm");
+    });
+    $("#videoLeccion_04_4").on("click", function(){
+        $("#video").attr("src", "sistema_serna2/SISTEMA SERNA_I/VIDEOS/Nivel_04/04/04_SS_Cantar_FA_04.webm");
+    });
+    $("#videoLeccion_04_5").on("click", function(){
+        $("#video").attr("src", "sistema_serna2/SISTEMA SERNA_I/VIDEOS/Nivel_04/04/05_SS_Escuchar _SOL y FA_04.webm");
+    });
+    $("#videoLeccion_04_6").on("click", function(){
+        $("#video").attr("src", "sistema_serna2/SISTEMA SERNA_I/VIDEOS/Nivel_04/04/06_SS_Cantar _SOL y FA_04.webm");
+    });
+
+    //04_a
+    $("#videoLeccion_4a_1").on("click", function () {
+        $("#video").attr("src", "sistema_serna2/SISTEMA SERNA_I/VIDEOS/Nivel_04/04_a/01_SS_Escuchar_SOL_04_a.webm");
+    });
+    $("#videoLeccion_4a_2").on("click", function () {
+        $("#video").attr("src", "sistema_serna2/SISTEMA SERNA_I/VIDEOS/Nivel_04/04_a/02_SS_Cantar_SOL_04_a.webm");
+    });
+    $("#videoLeccion_4a_3").on("click", function () {
+        $("#video").attr("src", "sistema_serna2/SISTEMA SERNA_I/VIDEOS/Nivel_04/04_a/03_SS_Escuchar_FA_04_a.webm");
+    });
+    $("#videoLeccion_4a_4").on("click", function () {
+        $("#video").attr("src", "sistema_serna2/SISTEMA SERNA_I/VIDEOS/Nivel_04/04_a/04_SS_Cantar_FA_04_a.webm");
+    });
+    $("#videoLeccion_4a_5").on("click", function () {
+        $("#video").attr("src", "sistema_serna2/SISTEMA SERNA_I/VIDEOS/Nivel_04/04_a/05_SS_Escuchar _SOL y FA_04_a.webm");
+    });
+    $("#videoLeccion_4a_6").on("click", function () {
+        $("#video").attr("src", "sistema_serna2/SISTEMA SERNA_I/VIDEOS/Nivel_04/04_a/06_SS_Cantar _SOL y FA_04_a.webm");
+    });  
+    
+    //04_b
+    $("#videoLeccion_4b_1").on("click", function () {
+        $("#video").attr("src", "sistema_serna2/SISTEMA SERNA_I/VIDEOS/Nivel_04/04_b/01_SS_Escuchar_SOL_04_b.webm");
+    });
+    $("#videoLeccion_4b_2").on("click", function () {
+        $("#video").attr("src", "sistema_serna2/SISTEMA SERNA_I/VIDEOS/Nivel_04/04_b/02_SS_Cantar_SOL_04_b.webm");
+    });
+    $("#videoLeccion_4b_3").on("click", function () {
+        $("#video").attr("src", "sistema_serna2/SISTEMA SERNA_I/VIDEOS/Nivel_04/04_b/03_SS_Escuchar_FA_04_b.webm");
+    });
+    $("#videoLeccion_4b_4").on("click", function () {
+        $("#video").attr("src", "sistema_serna2/SISTEMA SERNA_I/VIDEOS/Nivel_04/04_b/04_SS_Cantar_FA_04_b.webm");
+    });
+    $("#videoLeccion_4b_5").on("click", function () {
+        $("#video").attr("src", "sistema_serna2/SISTEMA SERNA_I/VIDEOS/Nivel_04/04_b/05_SS_Escuchar _SOL y FA_04_b.webm");
+    });
+    $("#videoLeccion_4b_6").on("click", function () {
+        $("#video").attr("src", "sistema_serna2/SISTEMA SERNA_I/VIDEOS/Nivel_04/04_b/06_SS_Cantar _SOL y FA_04_b.webm");
+    });
+
+    //04_c
+    $("#videoLeccion_4c_1").on("click", function () {
+        $("#video").attr("src", "sistema_serna2/SISTEMA SERNA_I/VIDEOS/Nivel_04/04_c/01_SS_Escuchar_SOL_04_c.webm");
+    });
+    $("#videoLeccion_4c_2").on("click", function () {
+        $("#video").attr("src", "sistema_serna2/SISTEMA SERNA_I/VIDEOS/Nivel_04/04_c/02_SS_Cantar_SOL_04_c.webm");
+    });
+    $("#videoLeccion_4c_3").on("click", function () {
+        $("#video").attr("src", "sistema_serna2/SISTEMA SERNA_I/VIDEOS/Nivel_04/04_c/03_SS_Escuchar_FA_04_c.webm");
+    });
+    $("#videoLeccion_4c_4").on("click", function () {
+        $("#video").attr("src", "sistema_serna2/SISTEMA SERNA_I/VIDEOS/Nivel_04/04_c/04_SS_Cantar_FA_04_c.webm");
+    });
+    $("#videoLeccion_4c_5").on("click", function () {
+        $("#video").attr("src", "sistema_serna2/SISTEMA SERNA_I/VIDEOS/Nivel_04/04_c/05_SS_Escuchar _SOL y FA_04_c.webm");
+    });
+    $("#videoLeccion_4c_6").on("click", function () {
+        $("#video").attr("src", "sistema_serna2/SISTEMA SERNA_I/VIDEOS/Nivel_04/04_c/06_SS_Cantar _SOL y FA_04_c.webm");
+    });
+
+    //---
+
+    //05
+    $("#videoLeccion_05_1").on("click", function () {
+        $("#video").attr("src", "sistema_serna2/SISTEMA SERNA_I/VIDEOS/Nivel_05/05/01_SS_Escuchar_SOL_05.webm");
+    });
+    $("#videoLeccion_05_2").on("click", function () {
+        $("#video").attr("src", "sistema_serna2/SISTEMA SERNA_I/VIDEOS/Nivel_05/05/02_SS_Cantar_SOL_05.webm");
+    });
+    $("#videoLeccion_05_3").on("click", function () {
+        $("#video").attr("src", "sistema_serna2/SISTEMA SERNA_I/VIDEOS/Nivel_05/05/03_SS_Escuchar_FA_05.webm");
+    });
+    $("#videoLeccion_05_4").on("click", function () {
+        $("#video").attr("src", "sistema_serna2/SISTEMA SERNA_I/VIDEOS/Nivel_05/05/04_SS_Cantar_FA_05.webm");
+    });
+    $("#videoLeccion_05_5").on("click", function () {
+        $("#video").attr("src", "sistema_serna2/SISTEMA SERNA_I/VIDEOS/Nivel_05/05/05_SS_Escuchar _SOL y FA_05.webm");
+    });
+    $("#videoLeccion_05_6").on("click", function () {
+        $("#video").attr("src", "sistema_serna2/SISTEMA SERNA_I/VIDEOS/Nivel_05/05/06_SS_Cantar _SOL y FA_05.webm");
+    });
+
+    //05_a
+    $("#videoLeccion_5a_1").on("click", function () {
+        $("#video").attr("src", "sistema_serna2/SISTEMA SERNA_I/VIDEOS/Nivel_05/05_a/01_SS_Escuchar_SOL_05_a.webm");
+    });
+    $("#videoLeccion_5a_2").on("click", function () {
+        $("#video").attr("src", "sistema_serna2/SISTEMA SERNA_I/VIDEOS/Nivel_05/05_a/02_SS_Cantar_SOL_05_a.webm");
+    });
+    $("#videoLeccion_5a_3").on("click", function () {
+        $("#video").attr("src", "sistema_serna2/SISTEMA SERNA_I/VIDEOS/Nivel_05/05_a/03_SS_Escuchar_FA_05_a.webm");
+    });
+    $("#videoLeccion_5a_4").on("click", function () {
+        $("#video").attr("src", "sistema_serna2/SISTEMA SERNA_I/VIDEOS/Nivel_05/05_a/04_SS_Cantar_FA_05_a.webm");
+    });
+    $("#videoLeccion_5a_5").on("click", function () {
+        $("#video").attr("src", "sistema_serna2/SISTEMA SERNA_I/VIDEOS/Nivel_05/05_a/05_SS_Escuchar _SOL y FA_05_a.webm");
+    });
+    $("#videoLeccion_5a_6").on("click", function () {
+        $("#video").attr("src", "sistema_serna2/SISTEMA SERNA_I/VIDEOS/Nivel_05/05_a/06_SS_Cantar _SOL y FA_05_a.webm");
+    });
+
+    //05_b
+    $("#videoLeccion_5b_1").on("click", function () {
+        $("#video").attr("src", "sistema_serna2/SISTEMA SERNA_I/VIDEOS/Nivel_05/05_b/01_SS_Escuchar_SOL_05_b.webm");
+    });
+    $("#videoLeccion_5b_2").on("click", function () {
+        $("#video").attr("src", "sistema_serna2/SISTEMA SERNA_I/VIDEOS/Nivel_05/05_b/02_SS_Cantar_SOL_05_b.webm");
+    });
+    $("#videoLeccion_5b_3").on("click", function () {
+        $("#video").attr("src", "sistema_serna2/SISTEMA SERNA_I/VIDEOS/Nivel_05/05_b/03_SS_Escuchar_FA_05_b.webm");
+    });
+    $("#videoLeccion_5b_4").on("click", function () {
+        $("#video").attr("src", "sistema_serna2/SISTEMA SERNA_I/VIDEOS/Nivel_05/05_b/04_SS_Cantar_FA_05_b.webm");
+    });
+    $("#videoLeccion_5b_5").on("click", function () {
+        $("#video").attr("src", "sistema_serna2/SISTEMA SERNA_I/VIDEOS/Nivel_05/05_b/05_SS_Escuchar _SOL y FA_05_b.webm");
+    });
+    $("#videoLeccion_5b_6").on("click", function () {
+        $("#video").attr("src", "sistema_serna2/SISTEMA SERNA_I/VIDEOS/Nivel_05/05_b/06_SS_Cantar _SOL y FA_05_b.webm");
+    });
+
+    //05_c
+    $("#videoLeccion_5c_1").on("click", function () {
+        $("#video").attr("src", "sistema_serna2/SISTEMA SERNA_I/VIDEOS/Nivel_05/05_c/01_SS_Escuchar_SOL_05_c.webm");
+    });
+    $("#videoLeccion_5c_2").on("click", function () {
+        $("#video").attr("src", "sistema_serna2/SISTEMA SERNA_I/VIDEOS/Nivel_05/05_c/02_SS_Cantar_SOL_05_c.webm");
+    });
+    $("#videoLeccion_5c_3").on("click", function () {
+        $("#video").attr("src", "sistema_serna2/SISTEMA SERNA_I/VIDEOS/Nivel_05/05_c/03_SS_Escuchar_FA_05_c.webm");
+    });
+    $("#videoLeccion_5c_4").on("click", function () {
+        $("#video").attr("src", "sistema_serna2/SISTEMA SERNA_I/VIDEOS/Nivel_05/05_c/04_SS_Cantar_FA_05_c.webm");
+    });
+    $("#videoLeccion_5c_5").on("click", function () {
+        $("#video").attr("src", "sistema_serna2/SISTEMA SERNA_I/VIDEOS/Nivel_05/05_c/05_SS_Escuchar _SOL y FA_05_c.webm");
+    });
+    $("#videoLeccion_5c_6").on("click", function () {
+        $("#video").attr("src", "sistema_serna2/SISTEMA SERNA_I/VIDEOS/Nivel_05/05_c/06_SS_Cantar _SOL y FA_05_c.webm");
+    });
+
+    //---
+
+    //06
+    $("#videoLeccion_06_1").on("click", function () {
+        $("#video").attr("src", "sistema_serna2/SISTEMA SERNA_I/VIDEOS/Nivel_06/06/01_SS_Escuchar_SOL_06.webm");
+    });
+    $("#videoLeccion_06_2").on("click", function () {
+        $("#video").attr("src", "sistema_serna2/SISTEMA SERNA_I/VIDEOS/Nivel_06/06/02_SS_Cantar_SOL_06.webm");
+    });
+    $("#videoLeccion_06_3").on("click", function () {
+        $("#video").attr("src", "sistema_serna2/SISTEMA SERNA_I/VIDEOS/Nivel_06/06/03_SS_Escuchar_FA_06.webm");
+    });
+    $("#videoLeccion_06_4").on("click", function () {
+        $("#video").attr("src", "sistema_serna2/SISTEMA SERNA_I/VIDEOS/Nivel_06/06/04_SS_Cantar_FA_06.webm");
+    });
+    $("#videoLeccion_06_5").on("click", function () {
+        $("#video").attr("src", "sistema_serna2/SISTEMA SERNA_I/VIDEOS/Nivel_06/06/05_SS_Escuchar _SOL y FA_06.webm");
+    });
+    $("#videoLeccion_06_6").on("click", function () {
+        $("#video").attr("src", "sistema_serna2/SISTEMA SERNA_I/VIDEOS/Nivel_06/06/06_SS_Cantar _SOL y FA_06.webm");
+    });
+
+    //06_a
+    $("#videoLeccion_6a_1").on("click", function () {
+        $("#video").attr("src", "sistema_serna2/SISTEMA SERNA_I/VIDEOS/Nivel_06/06_a/01_SS_Escuchar_SOL_06_a.webm");
+    });
+    $("#videoLeccion_6a_2").on("click", function () {
+        $("#video").attr("src", "sistema_serna2/SISTEMA SERNA_I/VIDEOS/Nivel_06/06_a/02_SS_Cantar_SOL_06_a.webm");
+    });
+    $("#videoLeccion_6a_3").on("click", function () {
+        $("#video").attr("src", "sistema_serna2/SISTEMA SERNA_I/VIDEOS/Nivel_06/06_a/03_SS_Escuchar_FA_06_a.webm");
+    });
+    $("#videoLeccion_6a_4").on("click", function () {
+        $("#video").attr("src", "sistema_serna2/SISTEMA SERNA_I/VIDEOS/Nivel_06/06_a/04_SS_Cantar_FA_06_a.webm");
+    });
+    $("#videoLeccion_6a_5").on("click", function () {
+        $("#video").attr("src", "sistema_serna2/SISTEMA SERNA_I/VIDEOS/Nivel_06/06_a/05_SS_Escuchar _SOL y FA_06_a.webm");
+    });
+    $("#videoLeccion_6a_6").on("click", function () {
+        $("#video").attr("src", "sistema_serna2/SISTEMA SERNA_I/VIDEOS/Nivel_06/06_a/06_SS_Cantar _SOL y FA_06_a.webm");
+    });
+
+    //06_b
+    $("#videoLeccion_6b_1").on("click", function () {
+        $("#video").attr("src", "sistema_serna2/SISTEMA SERNA_I/VIDEOS/Nivel_06/06_b/01_SS_Escuchar_SOL_06_b.webm");
+    });
+    $("#videoLeccion_6b_2").on("click", function () {
+        $("#video").attr("src", "sistema_serna2/SISTEMA SERNA_I/VIDEOS/Nivel_06/06_b/02_SS_Cantar_SOL_06_b.webm");
+    });
+    $("#videoLeccion_6b_3").on("click", function () {
+        $("#video").attr("src", "sistema_serna2/SISTEMA SERNA_I/VIDEOS/Nivel_06/06_b/03_SS_Escuchar_FA_06_b.webm");
+    });
+    $("#videoLeccion_6b_4").on("click", function () {
+        $("#video").attr("src", "sistema_serna2/SISTEMA SERNA_I/VIDEOS/Nivel_06/06_b/04_SS_Cantar_FA_06_b.webm");
+    });
+    $("#videoLeccion_6b_5").on("click", function () {
+        $("#video").attr("src", "sistema_serna2/SISTEMA SERNA_I/VIDEOS/Nivel_06/06_b/05_SS_Escuchar _SOL y FA_06_b.webm");
+    });
+    $("#videoLeccion_6b_6").on("click", function () {
+        $("#video").attr("src", "sistema_serna2/SISTEMA SERNA_I/VIDEOS/Nivel_06/06_b/06_SS_Cantar _SOL y FA_06_b.webm");
+    });
+
+    //06_c
+    $("#videoLeccion_6c_1").on("click", function () {
+        $("#video").attr("src", "sistema_serna2/SISTEMA SERNA_I/VIDEOS/Nivel_06/06_c/01_SS_Escuchar_SOL_06_c.webm");
+    });
+    $("#videoLeccion_6c_2").on("click", function () {
+        $("#video").attr("src", "sistema_serna2/SISTEMA SERNA_I/VIDEOS/Nivel_06/06_c/02_SS_Cantar_SOL_06_c.webm");
+    });
+    $("#videoLeccion_6c_3").on("click", function () {
+        $("#video").attr("src", "sistema_serna2/SISTEMA SERNA_I/VIDEOS/Nivel_06/06_c/03_SS_Escuchar_FA_06_c.webm");
+    });
+    $("#videoLeccion_6c_4").on("click", function () {
+        $("#video").attr("src", "sistema_serna2/SISTEMA SERNA_I/VIDEOS/Nivel_06/06_c/04_SS_Cantar_FA_06_c.webm");
+    });
+    $("#videoLeccion_6c_5").on("click", function () {
+        $("#video").attr("src", "sistema_serna2/SISTEMA SERNA_I/VIDEOS/Nivel_06/06_c/05_SS_Escuchar _SOL y FA_06_c.webm");
+    });
+    $("#videoLeccion_6c_6").on("click", function () {
+        $("#video").attr("src", "sistema_serna2/SISTEMA SERNA_I/VIDEOS/Nivel_06/06_c/06_SS_Cantar _SOL y FA_06_c.webm");
+    });
+
+    //---
+
+    //07
+    $("#videoLeccion_07_1").on("click", function () {
+        $("#video").attr("src", "sistema_serna2/SISTEMA SERNA_I/VIDEOS/Nivel_07/07/01_SS_Escuchar_SOL_07.webm");
+    });
+    $("#videoLeccion_07_2").on("click", function () {
+        $("#video").attr("src", "sistema_serna2/SISTEMA SERNA_I/VIDEOS/Nivel_07/07/02_SS_Cantar_SOL_07.webm");
+    });
+    $("#videoLeccion_07_3").on("click", function () {
+        $("#video").attr("src", "sistema_serna2/SISTEMA SERNA_I/VIDEOS/Nivel_07/07/03_SS_Escuchar_FA_07.webm");
+    });
+    $("#videoLeccion_07_4").on("click", function () {
+        $("#video").attr("src", "sistema_serna2/SISTEMA SERNA_I/VIDEOS/Nivel_07/07/04_SS_Cantar_FA_07.webm");
+    });
+    $("#videoLeccion_07_5").on("click", function () {
+        $("#video").attr("src", "sistema_serna2/SISTEMA SERNA_I/VIDEOS/Nivel_07/07/05_SS_Escuchar _SOL y FA_07.webm");
+    });
+    $("#videoLeccion_07_6").on("click", function () {
+        $("#video").attr("src", "sistema_serna2/SISTEMA SERNA_I/VIDEOS/Nivel_07/07/06_SS_Cantar _SOL y FA_07.webm");
+    });
+
+    //07_a
+    $("#videoLeccion_7a_1").on("click", function () {
+        $("#video").attr("src", "sistema_serna2/SISTEMA SERNA_I/VIDEOS/Nivel_07/07_a/01_SS_Escuchar_SOL_07_a.webm");
+    });
+    $("#videoLeccion_7a_2").on("click", function () {
+        $("#video").attr("src", "sistema_serna2/SISTEMA SERNA_I/VIDEOS/Nivel_07/07_a/02_SS_Cantar_SOL_07_a.webm");
+    });
+    $("#videoLeccion_7a_3").on("click", function () {
+        $("#video").attr("src", "sistema_serna2/SISTEMA SERNA_I/VIDEOS/Nivel_07/07_a/03_SS_Escuchar_FA_07_a.webm");
+    });
+    $("#videoLeccion_7a_4").on("click", function () {
+        $("#video").attr("src", "sistema_serna2/SISTEMA SERNA_I/VIDEOS/Nivel_07/07_a/04_SS_Cantar_FA_07_a.webm");
+    });
+    $("#videoLeccion_7a_5").on("click", function () {
+        $("#video").attr("src", "sistema_serna2/SISTEMA SERNA_I/VIDEOS/Nivel_07/07_a/05_SS_Escuchar _SOL y FA_07_a.webm");
+    });
+    $("#videoLeccion_7a_6").on("click", function () {
+        $("#video").attr("src", "sistema_serna2/SISTEMA SERNA_I/VIDEOS/Nivel_07/07_a/06_SS_Cantar _SOL y FA_07_a.webm");
+    });
+
+    //07_b
+    $("#videoLeccion_7b_1").on("click", function () {
+        $("#video").attr("src", "sistema_serna2/SISTEMA SERNA_I/VIDEOS/Nivel_07/07_b/01_SS_Escuchar_SOL_07_b.webm");
+    });
+    $("#videoLeccion_7b_2").on("click", function () {
+        $("#video").attr("src", "sistema_serna2/SISTEMA SERNA_I/VIDEOS/Nivel_07/07_b/02_SS_Cantar_SOL_07_b.webm");
+    });
+    $("#videoLeccion_7b_3").on("click", function () {
+        $("#video").attr("src", "sistema_serna2/SISTEMA SERNA_I/VIDEOS/Nivel_07/07_b/03_SS_Escuchar_FA_07_b.webm");
+    });
+    $("#videoLeccion_7b_4").on("click", function () {
+        $("#video").attr("src", "sistema_serna2/SISTEMA SERNA_I/VIDEOS/Nivel_07/07_b/04_SS_Cantar_FA_07_b.webm");
+    });
+    $("#videoLeccion_7b_5").on("click", function () {
+        $("#video").attr("src", "sistema_serna2/SISTEMA SERNA_I/VIDEOS/Nivel_07/07_b/05_SS_Escuchar _SOL y FA_07_b.webm");
+    });
+    $("#videoLeccion_7b_6").on("click", function () {
+        $("#video").attr("src", "sistema_serna2/SISTEMA SERNA_I/VIDEOS/Nivel_07/07_b/06_SS_Cantar _SOL y FA_07_b.webm");
+    });
+
+    //07_c
+    $("#videoLeccion_7c_1").on("click", function () {
+        $("#video").attr("src", "sistema_serna2/SISTEMA SERNA_I/VIDEOS/Nivel_07/07_c/01_SS_Escuchar_SOL_07_c.webm");
+    });
+    $("#videoLeccion_7c_2").on("click", function () {
+        $("#video").attr("src", "sistema_serna2/SISTEMA SERNA_I/VIDEOS/Nivel_07/07_c/02_SS_Cantar_SOL_07_c.webm");
+    });
+    $("#videoLeccion_7c_3").on("click", function () {
+        $("#video").attr("src", "sistema_serna2/SISTEMA SERNA_I/VIDEOS/Nivel_07/07_c/03_SS_Escuchar_FA_07_c.webm");
+    });
+    $("#videoLeccion_7c_4").on("click", function () {
+        $("#video").attr("src", "sistema_serna2/SISTEMA SERNA_I/VIDEOS/Nivel_07/07_c/04_SS_Cantar_FA_07_c.webm");
+    });
+    $("#videoLeccion_7c_5").on("click", function () {
+        $("#video").attr("src", "sistema_serna2/SISTEMA SERNA_I/VIDEOS/Nivel_07/07_c/05_SS_Escuchar _SOL y FA_07_c.webm");
+    });
+    $("#videoLeccion_7c_6").on("click", function () {
+        $("#video").attr("src", "sistema_serna2/SISTEMA SERNA_I/VIDEOS/Nivel_07/07_c/06_SS_Cantar _SOL y FA_07_c.webm");
+    });
+});
