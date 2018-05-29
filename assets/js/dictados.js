0547100b8ef7a874c598e24f02a0a8d34363508a
Lalo Luna <thelalox789@gmail.com>
2018-05-29 10:25:25 -0500

    Modulo
    Secorrigieron los errores que se tenian ademas de agregar una pagina para ir a cada actividad y se termino con las carpetas del sistema-serna

---
+++ assets/js/dictados.js
@@ -0,0 +1,110 @@
+$(document).ready(function(){
+	
+    //Practima m
+    $("#acordemayor_01").on("click", function(){
+        $("#video").attr("src", "sistema_serna2/SISTEMA SERNA_I/SS_Dictados/04_Acordes_Mayor_m/Practica_M_m/01_Practica_M_m.webm");
+    });
+    $("#acordemayor_02").on("click", function(){
+        $("#video").attr("src", "sistema_serna2/SISTEMA SERNA_I/SS_Dictados/04_Acordes_Mayor_m/Practica_M_m/02_Practica_M_m.webm");
+    });
+    $("#acordemayor_03").on("click", function(){
+        $("#video").attr("src", "sistema_serna2/SISTEMA SERNA_I/SS_Dictados/04_Acordes_Mayor_m/Practica_M_m/03_Practica_M_m.webm");
+    });
+    $("#acordemayor_04").on("click", function(){
+        $("#video").attr("src", "sistema_serna2/SISTEMA SERNA_I/SS_Dictados/04_Acordes_Mayor_m/Practica_M_m/04_Practica_M_m.webm");
+    });
+    $("#acordemayor_05").on("click", function(){
+        $("#video").attr("src", "sistema_serna2/SISTEMA SERNA_I/SS_Dictados/04_Acordes_Mayor_m/Practica_M_m/05_Practica_M_m.webm");
+    });
+    $("#acordemayor_06").on("click", function(){
+        $("#video").attr("src", "sistema_serna2/SISTEMA SERNA_I/SS_Dictados/04_Acordes_Mayor_m/Practica_M_m/06_Practica_M_m.webm");
+    });
+    $("#acordemayor_07").on("click", function(){
+        $("#video").attr("src", "sistema_serna2/SISTEMA SERNA_I/SS_Dictados/04_Acordes_Mayor_m/Practica_M_m/07_Practica_M_m.webm");
+    });
+    $("#acordemayor_08").on("click", function(){
+        $("#video").attr("src", "sistema_serna2/SISTEMA SERNA_I/SS_Dictados/04_Acordes_Mayor_m/Practica_M_m/08_Practica_M_m.webm");
+    });
+    $("#acordemayor_09").on("click", function(){
+        $("#video").attr("src", "sistema_serna2/SISTEMA SERNA_I/SS_Dictados/04_Acordes_Mayor_m/Practica_M_m/09_Practica_M_m.webm");
+    });
+    $("#acordemayor_10").on("click", function(){
+        $("#video").attr("src", "sistema_serna2/SISTEMA SERNA_I/SS_Dictados/04_Acordes_Mayor_m/Practica_M_m/10_Practica_M_m.webm");
+    });
+    $("#acordemayor_11").on("click", function(){
+        $("#video").attr("src", "sistema_serna2/SISTEMA SERNA_I/SS_Dictados/04_Acordes_Mayor_m/Practica_M_m/11_Practica_M_m.webm");
+    });
+    $("#acordemayor_12").on("click", function(){
+        $("#video").attr("src", "sistema_serna2/SISTEMA SERNA_I/SS_Dictados/04_Acordes_Mayor_m/Practica_M_m/12_Practica_M_m.webm");
+    });
+    $("#acordemayor_13").on("click", function(){
+        $("#video").attr("src", "sistema_serna2/SISTEMA SERNA_I/SS_Dictados/04_Acordes_Mayor_m/Practica_M_m/13_Practica_M_m.webm");
+    });
+
+    //Funciones armonicas
+    $("#armonica_01").on("click", function(){
+        $("#video").attr("src", "sistema_serna2/SISTEMA SERNA_I/SS_Dictados/05_Funciiones Armonicas_I_IV_V/01_Funciones Armonicas_I_IV_V.webm");
+    });
+    $("#armonica_02").on("click", function(){
+        $("#video").attr("src", "sistema_serna2/SISTEMA SERNA_I/SS_Dictados/05_Funciiones Armonicas_I_IV_V/02_Funciones Armonicas_I_IV_V.webm");
+    });
+    $("#armonica_03").on("click", function(){
+        $("#video").attr("src", "sistema_serna2/SISTEMA SERNA_I/SS_Dictados/05_Funciiones Armonicas_I_IV_V/03_Funciones Armonicas_I_IV_V.webm");
+    });
+    $("#armonica_04").on("click", function(){
+        $("#video").attr("src", "sistema_serna2/SISTEMA SERNA_I/SS_Dictados/05_Funciiones Armonicas_I_IV_V/04_Funciones Armonicas_I_IV_V.webm");
+    });
+    $("#armonica_05").on("click", function(){
+        $("#video").attr("src", "sistema_serna2/SISTEMA SERNA_I/SS_Dictados/05_Funciiones Armonicas_I_IV_V/05_Funciones Armonicas_I_IV_V.webm");
+    });
+    $("#armonica_06").on("click", function(){
+        $("#video").attr("src", "sistema_serna2/SISTEMA SERNA_I/SS_Dictados/05_Funciiones Armonicas_I_IV_V/06_Funciones Armonicas_I_IV_V.webm");
+    });
+    $("#armonica_07").on("click", function(){
+        $("#video").attr("src", "sistema_serna2/SISTEMA SERNA_I/SS_Dictados/05_Funciiones Armonicas_I_IV_V/07_Funciones Armonicas_I_IV_V.webm");
+    });
+    $("#armonica_08").on("click", function(){
+        $("#video").attr("src", "sistema_serna2/SISTEMA SERNA_I/SS_Dictados/05_Funciiones Armonicas_I_IV_V/08_Funciones Armonicas_I_IV_V.webm");
+    });
+    $("#armonica_09").on("click", function(){
+        $("#video").attr("src", "sistema_serna2/SISTEMA SERNA_I/SS_Dictados/05_Funciiones Armonicas_I_IV_V/09_Funciones Armonicas_I_IV_V.webm");
+    });
+    $("#armonica_10").on("click", function(){
+        $("#video").attr("src", "sistema_serna2/SISTEMA SERNA_I/SS_Dictados/05_Funciiones Armonicas_I_IV_V/10_Funciones Armonicas_I_IV_V.webm");
+    });
+    $("#armonica_11").on("click", function(){
+        $("#video").attr("src", "sistema_serna2/SISTEMA SERNA_I/SS_Dictados/05_Funciiones Armonicas_I_IV_V/11_Funciones Armonicas_I_IV_V.webm");
+    });
+    $("#armonica_12").on("click", function(){
+        $("#video").attr("src", "sistema_serna2/SISTEMA SERNA_I/SS_Dictados/05_Funciiones Armonicas_I_IV_V/12_Funciones Armonicas_I_IV_V.webm");
+    });
+    $("#armonica_13").on("click", function(){
+        $("#video").attr("src", "sistema_serna2/SISTEMA SERNA_I/SS_Dictados/05_Funciiones Armonicas_I_IV_V/13_Funciones Armonicas_I_IV_V.webm");
+    });
+    $("#armonica_14").on("click", function(){
+        $("#video").attr("src", "sistema_serna2/SISTEMA SERNA_I/SS_Dictados/05_Funciiones Armonicas_I_IV_V/14_Funciones Armonicas_I_IV_V.webm");
+    });
+    $("#armonica_15").on("click", function(){
+        $("#video").attr("src", "sistema_serna2/SISTEMA SERNA_I/SS_Dictados/05_Funciiones Armonicas_I_IV_V/15_Funciones Armonicas_I_IV_V.webm");
+    });
+    $("#armonica_16").on("click", function(){
+        $("#video").attr("src", "sistema_serna2/SISTEMA SERNA_I/SS_Dictados/05_Funciiones Armonicas_I_IV_V/16_Funciones Armonicas_I_IV_V.webm");
+    });
+
+    $("#armonica_17").on("click", function(){
+        $("#video").attr("src", "sistema_serna2/SISTEMA SERNA_I/SS_Dictados/05_Funciiones Armonicas_I_IV_V/con IV Menor en modo Mayor/17_Funciones Armonicas_I_IV_V.webm");
+    });
+    $("#armonica_18").on("click", function(){
+        $("#video").attr("src", "sistema_serna2/SISTEMA SERNA_I/SS_Dictados/05_Funciiones Armonicas_I_IV_V/con IV Menor en modo Mayor/18_Funciones Armonicas_I_IV_V.webm");
+    });
+    $("#armonica_19").on("click", function(){
+        $("#video").attr("src", "sistema_serna2/SISTEMA SERNA_I/SS_Dictados/05_Funciiones Armonicas_I_IV_V/con IV Menor en modo Mayor/19_Funciones Armonicas_I_IV_V.webm");
+    });
+    $("#armonica_20").on("click", function(){
+        $("#video").attr("src", "sistema_serna2/SISTEMA SERNA_I/SS_Dictados/05_Funciiones Armonicas_I_IV_V/con IV Menor en modo Mayor/20_Funciones Armonicas_I_IV_V.webm");
+    });
+    $("#armonica_21").on("click", function(){
+        $("#video").attr("src", "sistema_serna2/SISTEMA SERNA_I/SS_Dictados/05_Funciiones Armonicas_I_IV_V/con IV Menor en modo Mayor/21_Funciones Armonicas_I_IV_V.webm");
+    });
+
+});
---
+++ assets/js/notas.js
@@ -0,0 +1,101 @@
+$(document).ready(function(){
+    //Nivel 2
+    $("#nota_02_1").on("click", function () {
+        $("audio").attr("src", "sistema_serna2/SISTEMA SERNA_I/SS_Decir la Nota/Nivel 2/01_Sistema Serna_D_la_ Nota_Nivel_02.mp3");
+    });
+    $("#nota_02_2").on("click", function () {
+        $("audio").attr("src", "sistema_serna2/SISTEMA SERNA_I/SS_Decir la Nota/Nivel 2/02_Sistema Serna_D_la_ Nota_Nivel_02.mp3");
+    });
+    $("#nota_02_3").on("click", function () {
+        $("audio").attr("src", "sistema_serna2/SISTEMA SERNA_I/SS_Decir la Nota/Nivel 2/03_Sistema Serna_D_la_ Nota_Nivel_02.mp3");
+    });
+    $("#nota_02_4").on("click", function () {
+        $("audio").attr("src", "sistema_serna2/SISTEMA SERNA_I/SS_Decir la Nota/Nivel 2/04_Sistema Serna_D_la_ Nota_Nivel_02.mp3");
+    });
+    $("#nota_02_5").on("click", function () {
+        $("audio").attr("src", "sistema_serna2/SISTEMA SERNA_I/SS_Decir la Nota/Nivel 2/05_Sistema Serna_D_la_ Nota_Nivel_02.mp3");
+    });
+    $("#nota_02_6").on("click", function () {
+        $("audio").attr("src", "sistema_serna2/SISTEMA SERNA_I/SS_Decir la Nota/Nivel 2/06_Sistema Serna_D_la_ Nota_Nivel_02.mp3");
+    });
+
+    //Nivel 3
+    $("#nota_03_1").on("click", function () {
+        $("audio").attr("src", "sistema_serna2/SISTEMA SERNA_I/SS_Decir la Nota/Nivel 3/01_Sistema Serna_D_la_ Nota_Nivel_03.mp3");
+    });
+    $("#nota_03_2").on("click", function () {
+        $("audio").attr("src", "sistema_serna2/SISTEMA SERNA_I/SS_Decir la Nota/Nivel 3/02_Sistema Serna_D_la_ Nota_Nivel_03.mp3");
+    });
+    $("#nota_03_3").on("click", function () {
+        $("audio").attr("src", "sistema_serna2/SISTEMA SERNA_I/SS_Decir la Nota/Nivel 3/03_Sistema Serna_D_la_ Nota_Nivel_03.mp3");
+    });
+    $("#nota_03_4").on("click", function () {
+        $("audio").attr("src", "sistema_serna2/SISTEMA SERNA_I/SS_Decir la Nota/Nivel 3/04_Sistema Serna_D_la_ Nota_Nivel_03.mp3");
+    });
+    $("#nota_03_5").on("click", function () {
+        $("audio").attr("src", "sistema_serna2/SISTEMA SERNA_I/SS_Decir la Nota/Nivel 3/05_Sistema Serna_D_la_ Nota_Nivel_03.mp3");
+    });
+    $("#nota_03_6").on("click", function () {
+        $("audio").attr("src", "sistema_serna2/SISTEMA SERNA_I/SS_Decir la Nota/Nivel 3/06_Sistema Serna_D_la_ Nota_Nivel_03.mp3");
+    });
+
+    //Nivel 4
+    $("#nota_04_1").on("click", function () {
+        $("audio").attr("src", "sistema_serna2/SISTEMA SERNA_I/SS_Decir la Nota/Nivel 4/01_Sistema Serna_D_la_ Nota_Nivel_04.mp3");
+    });
+    $("#nota_04_2").on("click", function () {
+        $("audio").attr("src", "sistema_serna2/SISTEMA SERNA_I/SS_Decir la Nota/Nivel 4/02_Sistema Serna_D_la_ Nota_Nivel_04.mp3");
+    });
+    $("#nota_04_3").on("click", function () {
+        $("audio").attr("src", "sistema_serna2/SISTEMA SERNA_I/SS_Decir la Nota/Nivel 4/03_Sistema Serna_D_la_ Nota_Nivel_04.mp3");
+    });
+    $("#nota_04_4").on("click", function () {
+        $("audio").attr("src", "sistema_serna2/SISTEMA SERNA_I/SS_Decir la Nota/Nivel 4/04_Sistema Serna_D_la_ Nota_Nivel_04.mp3");
+    });
+    $("#nota_04_5").on("click", function () {
+        $("audio").attr("src", "sistema_serna2/SISTEMA SERNA_I/SS_Decir la Nota/Nivel 4/05_Sistema Serna_D_la_ Nota_Nivel_04.mp3");
+    });
+    $("#nota_04_6").on("click", function () {
+        $("audio").attr("src", "sistema_serna2/SISTEMA SERNA_I/SS_Decir la Nota/Nivel 4/06_Sistema Serna_D_la_ Nota_Nivel_04.mp3");
+    });
+
+    //Nivel 5
+    $("#nota_05_1").on("click", function () {
+        $("audio").attr("src", "sistema_serna2/SISTEMA SERNA_I/SS_Decir la Nota/Nivel 5/01_Sistema Serna_D_la_ Nota_Nivel_05.mp3");
+    });
+    $("#nota_05_2").on("click", function () {
+        $("audio").attr("src", "sistema_serna2/SISTEMA SERNA_I/SS_Decir la Nota/Nivel 5/02_Sistema Serna_D_la_ Nota_Nivel_05.mp3");
+    });
+    $("#nota_05_3").on("click", function () {
+        $("audio").attr("src", "sistema_serna2/SISTEMA SERNA_I/SS_Decir la Nota/Nivel 5/03_Sistema Serna_D_la_ Nota_Nivel_05.mp3");
+    });
+    $("#nota_05_4").on("click", function () {
+        $("audio").attr("src", "sistema_serna2/SISTEMA SERNA_I/SS_Decir la Nota/Nivel 5/04_Sistema Serna_D_la_ Nota_Nivel_05.mp3");
+    });
+    $("#nota_05_5").on("click", function () {
+        $("audio").attr("src", "sistema_serna2/SISTEMA SERNA_I/SS_Decir la Nota/Nivel 5/05_Sistema Serna_D_la_ Nota_Nivel_05.mp3");
+    });
+    $("#nota_05_6").on("click", function () {
+        $("audio").attr("src", "sistema_serna2/SISTEMA SERNA_I/SS_Decir la Nota/Nivel 5/06_Sistema Serna_D_la_ Nota_Nivel_05.mp3");
+    });
+    
+    //Nivel 6
+    $("#nota_06_1").on("click", function () {
+        $("audio").attr("src", "sistema_serna2/SISTEMA SERNA_I/SS_Decir la Nota/Nivel 6/01_Sistema Serna_D_la_ Nota_Nivel_06.mp3");
+    });
+    $("#nota_06_2").on("click", function () {
+        $("audio").attr("src", "sistema_serna2/SISTEMA SERNA_I/SS_Decir la Nota/Nivel 6/02_Sistema Serna_D_la_ Nota_Nivel_06.mp3");
+    });
+    $("#nota_06_3").on("click", function () {
+        $("audio").attr("src", "sistema_serna2/SISTEMA SERNA_I/SS_Decir la Nota/Nivel 6/03_Sistema Serna_D_la_ Nota_Nivel_06.mp3");
+    });
+    $("#nota_06_4").on("click", function () {
+        $("audio").attr("src", "sistema_serna2/SISTEMA SERNA_I/SS_Decir la Nota/Nivel 6/04_Sistema Serna_D_la_ Nota_Nivel_06.mp3");
+    });
+    $("#nota_06_5").on("click", function () {
+        $("audio").attr("src", "sistema_serna2/SISTEMA SERNA_I/SS_Decir la Nota/Nivel 6/05_Sistema Serna_D_la_ Nota_Nivel_06.mp3");
+    });
+    $("#nota_06_6").on("click", function () {
+        $("audio").attr("src", "sistema_serna2/SISTEMA SERNA_I/SS_Decir la Nota/Nivel 6/06_Sistema Serna_D_la_ Nota_Nivel_06.mp3");
+    });
+});
